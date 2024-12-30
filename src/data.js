f128ecc6591ff24e902f70d8fce305c85b6186df
kowsalyadeviPraveen <kowsipraveen1985@gmail.com>
2024-12-30 13:12:00 +0530

    second changes

---
+++ src/data.js
@@ -0,0 +1,34 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+
+
+
+
+const data = {
+    productData:[
+        {
+           id: 1,
+           img: img1,
+           title: 'Juicy Grapes',
+           desc: '',
+           price: 50,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Red Watermelon',
+            desc: '',
+            price: 57,
+         },
+         {
+            id: 3,
+            img: img3,
+            title: 'Juicy Orange',
+            desc: '',
+            price: 40,
+         },
+    ],
+};
+
+export default data;
